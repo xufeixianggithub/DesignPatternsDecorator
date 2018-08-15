8fac0fb691318cf7e54c8d164c2ef8bf9abff1f2
zude <zude@seeapp.com>
2018-08-15 18:14:52 +0800

    装饰模式，装饰奖金计算公式

--- src/main/java/com/xufx/ConcreteComponent/ConcreteComponent.java
+++ src/main/java/com/xufx/ConcreteComponent/ConcreteComponent.java
@@ -2,8 +2,11 @@ package com.xufx.ConcreteComponent;
 
 import com.xufx.Component.Component;
 
-public class ConcreteComponent extends Component{
-    public void operation() {
+import java.util.Date;
 
+public class ConcreteComponent extends Component{
+    public double calcPrize(String user, Date begin, Date end) {
+        //只是一个默认的实现，默认没有奖金
+        return 0;
     }
 }
--- src/main/java/com/xufx/Decorator/Decorator.java
+++ src/main/java/com/xufx/Decorator/Decorator.java
@@ -2,13 +2,16 @@ package com.xufx.Decorator;
 
 import com.xufx.Component.Component;
 
+import java.util.Date;
+
 public abstract class Decorator extends Component{
     protected Component component=null;
 
     public Decorator(Component component) {
         this.component=component;
     }
-    public  void operation(){
-        component.operation();
+    public double calcPrize(String user, Date begin, Date end) {
+        //转调组件对象的方法
+        return component.calcPrize(user, begin, end);
     }
 }
